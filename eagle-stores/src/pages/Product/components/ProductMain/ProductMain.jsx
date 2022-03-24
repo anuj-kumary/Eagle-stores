e9f9e56a92dcf416f61901fedc9b26b3292391aa
anuj-kumary <anujf0510@gmail.com>
2022-03-24 11:17:56 +0530

    feat : Made filter and search bar responsive

--- eagle-stores/src/pages/Product/components/ProductMain/ProductMain.jsx
+++ eagle-stores/src/pages/Product/components/ProductMain/ProductMain.jsx
@@ -1,16 +1,16 @@
-import "./ProductMain.css";
-import { useData } from "../../../../context/data/data-context";
-import { ProductCard } from "../ProductCard/ProductCard";
+import './ProductMain.css';
+import { useData } from '../../../../context/data/data-context';
+import { ProductCard } from '../ProductCard/ProductCard';
 
 export const ProductMain = () => {
   const { state } = useData();
 
   return (
     <>
-      <main className="product">
-        <h4 className="heading fw__500">
+      <main className='product'>
+        <h4 className='heading fw__500'>
           Showing All Products
-          <span className="product__subtext fw__400">
+          <span className='product__subtext fw__400'>
             (Showing {state.products.length} products)
           </span>
         </h4>
